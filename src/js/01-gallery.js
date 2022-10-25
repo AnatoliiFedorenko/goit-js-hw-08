24930b3d223d97521853178e7f56698baaf3c6db
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-25 10:11:41 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const gallery = galleryItems
+  .map(
+    image =>
+      ` <li>     
+            <a class="gallery__item" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" width="250" height="230"/>
+            </a>
+        </li>`
+  )
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', gallery);
+
+galleryContainer.addEventListener('click', bigPicture);
+
+function bigPicture(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
