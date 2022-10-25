e9436872e29d6df1d1e50456319c7948a62b2bb7
Anatolii Fedorenko <anatolyfedorenko@gmail.com>
2022-10-25 22:52:11 +0300

    Task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player/src/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    console.log(e.seconds);
+    localStorage.setItem('playerTime', e.seconds);
+  }, 100)
+);
+player
+  .setCurrentTime(localStorage.getItem('playerTime'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const Storage = 'feedback-form-state';
+
+if (localStorage.getItem(Storage)) {
+  const formData = JSON.parse(localStorage.getItem(Storage));
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
+
+form.addEventListener('input', throttle(saveData, 500));
+
+function saveData(e) {
+  const formData = JSON.parse(localStorage.getItem(Storage)) || {};
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(Storage, JSON.stringify(formData));
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault;
+  e.currentTarget.reset();
+  localStorage.removeItem(Storage);
+}
